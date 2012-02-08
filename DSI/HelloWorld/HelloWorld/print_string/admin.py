9070a36fd53af5b4feb2cbe7f956002277340837
Pedro Bilbao <pedro3900@gmail.com>
2012-02-08 12:47:59 +0000

    Borrando

--- DSI/HelloWorld/HelloWorld/print_string/admin.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 04/02/2012
-
-@author: victor
-'''
-from django.contrib import admin
-from models import HelloWorld
-
-admin.site.register(HelloWorld)
--- DSI/HelloWorld/HelloWorld/print_string/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class HelloWorld(models.Model):
-    string = models.CharField(max_length=255)
